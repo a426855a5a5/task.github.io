83f21cd0ad603d083712c78b99906e2c12cc68d8
Your Name <you@example.com>
2020-01-12 14:52:45 +0800

    done

---
+++ precache-manifest.87f0ba717dfd56d5c7045447ade6ba53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50efc1beea58174eef8a585b8cf26b8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25baf9284bfedaf57de8",
+    "url": "/static/css/main.5ed3a368.chunk.css"
+  },
+  {
+    "revision": "e0b054b0567286d46422",
+    "url": "/static/js/2.4c32e80a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4c32e80a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25baf9284bfedaf57de8",
+    "url": "/static/js/main.3c556dba.chunk.js"
+  },
+  {
+    "revision": "667a0fbb56fa14e1375c",
+    "url": "/static/js/runtime-main.ed3b4a2d.js"
+  }
+]);
